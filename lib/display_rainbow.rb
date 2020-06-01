7ae3de31ee993c269e988b86802218f12070e445
Nora Gaughan <noragaughan@gmail.com>
2020-06-01 22:03:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 
 def display_rainbow(colors)
-  puts "Red", "orange", "yellow", "green", "blue", "indigo", "violet"
+  puts colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 end
